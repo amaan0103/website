9dcc8246a4b6de9595d84dae6ed0fd2cdb698480
amaan0103 <73770856+amaan0103@users.noreply.github.com>
2021-09-04 12:03:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ $(document).click(function (event) {
         $navbar.collapse('hide');
     }
 });
+
+$('#gform').on('submit', function(e) {
+  var name=document.getElementById("entry.263992659").value;
+  var insert="Thank You " + name + "!!!";
+  $('#gform *').fadeOut(1000);
+  $('#feedback').fadeOut(1000);
+  setTimeout(function() {
+		document.getElementById("thankyouH2").innerHTML = insert;
+		$('#thankyou').fadeIn(1000);
+	  },1000);
+  setTimeout(function() {
+		document.getElementById("wink").className = "far fa-smile-wink fa-3x";
+		setTimeout(function() {
+		    document.getElementById("wink").className = "far fa-smile fa-3x";
+	    },1000);
+  },2000);
+});
